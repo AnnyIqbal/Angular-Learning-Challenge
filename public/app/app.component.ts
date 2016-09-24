bbbadb8630617b2ef2d427ef48a1aa572a704a46
Anny <quratulann_iqbal@hotmail.com>
2016-09-24 12:25:05 +0500

    `

--- public/app/app.component.ts
+++ public/app/app.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
 import { SearchBox } from './app.searchBox';
 import {SignIn} from './signin';
+import {Index1} from './index1';
 
 @Component({
   selector: 'my-app',
   template: `<signin></signin>`
-   //`<searchBox  text="Type Your Search Here"></searchBox>`
-  // (search)="onSearch($event)"
 })
-export class AppComponent { 
-  // onSearch(text) {
-  //   console.log(`From App : ${text}`);
-  // }
-}
+export class AppComponent {}
